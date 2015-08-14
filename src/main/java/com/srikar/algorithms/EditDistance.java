91917dd4c2eab61f2c28aa73bf458d5c6b6981ea
Frame02 <Frame02@users.noreply.github.com>
2015-08-14 12:07:03 +0530

    Initial commit

---
+++ src/main/java/com/srikar/algorithms/EditDistance.java
@@ -0,0 +1,13 @@
+package com.srikar.algorithms;
+
+/**
+ * Hello world!
+ *
+ */
+public class EditDistance 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
